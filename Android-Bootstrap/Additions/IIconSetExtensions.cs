1a09054424552a2a151993b1a63edb98beea1588
admin_dell <admin_dell@git.com>
2018-06-30 19:46:41 +0800

    first commit

---
+++ Android-Bootstrap/Additions/IIconSetExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Beardedhen.Androidbootstrap.Font
+{
+    public static partial class IIconSetExtensions
+    {
+        public static string FontPath(this Com.Beardedhen.Androidbootstrap.Font.IIconSet self)
+        {
+            global::Java.Lang.ICharSequence __result = self.FontPathFormatted();
+            var __rsval = __result?.ToString();
+            return __rsval;
+        }
+    }
+}
